916c2e32560b8fbf351cd71fde4755676fe040ff
Leon <leonsoshinski@gmail.com>
2024-03-29 21:37:24 +0300

    .

--- LS_messaging_system_abra/settings.py
+++ LS_messaging_system_abra/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = env("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lsassignmentserver.postgres.database.azure.com"]
 
